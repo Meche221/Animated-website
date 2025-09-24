035bf4f3c81b3bb5e4c1e4a913a44b0607109bae
Meche221 <binaymeche22@gmail.com>
2025-09-24 12:03:28 +0545

    gsap added

---
+++ index.js
@@ -0,0 +1,20 @@
+const videocon = document.querySelector("#video-container");
+const playbtn = document.querySelector("#play")
+videocon.addEventListener("mouseenter", () => {
+  gsap.to(playbtn, {
+    scale: 1,
+    opacity:1,
+  })
+})
+videocon.addEventListener("mouseleave", () => {
+  gsap.to(playbtn,{
+    opacity:0,
+    scale:0,
+  })
+})
+videocon.addEventListener("mousemove", (dets) => {
+  gsap.to(playbtn, {
+    left: dets.x-30,
+    top: dets.y-30,
+  })
+})
